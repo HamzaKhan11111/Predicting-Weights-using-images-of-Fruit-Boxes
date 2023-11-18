a67922079bafeb02b905145c5b5f428767cc2fe1
HamzaKhan11111 <hamza.k11111@gmail.com>
2023-11-18 12:09:45 +0500

    updaed Dataset.py and completed Regression_model.ipynb

--- Dataset.py
+++ Dataset.py
@@ -6,17 +6,19 @@ import torch
 import os
 
 class MyDataset(Dataset):
-    def __init__(self,csv_path):
+    def __init__(self,csv_path,transform=None):
         super().__init__
         csv=pd.read_csv(csv_path)
         self.img_path=csv.iloc[:,0].tolist()
         self.target=csv.iloc[:,1].tolist()
         self.total_images=len(csv)
+        self.transform=transform
 
     def __len__(self):
         return self.total_images
     
     def __getitem__(self, index):
-        img = torch.from_numpy(np.array(Image.open(os.path.join("Data", self.img_path[index])).convert("RGB"))).float()
-        target = self.target[index]
+      if self.transform:
+        img = self.transform(Image.open(os.path.join("Data", self.img_path[index])).convert("RGB"))
+        target = torch.tensor(self.target[index])
         return img, target
